200d48eeb9bf7cc5edfa65cfa05388774da1153e
Yang <luoxiaoyang0220@163.com>
2024-08-19 21:11:49 +0800

    init repository

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @Date: 2024/8/19 20:27
+ * @Author: Sean Luo
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
